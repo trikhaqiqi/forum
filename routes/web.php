e59bd42d4a979adbdd161405a1cecd6e563d810c
ilyassstri <trikhaqiqi@gmail.com>
2022-04-30 21:09:50 +0700

    forum episode 29

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\ReplyController;
+use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\ThreadController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('/dashboard', DashboardController::class)->name('dashboard');
@@ -15,5 +16,6 @@ Route::resource('threads', ThreadController::class);
 Route::post('threads/{thread:slug}/reply', [ReplyController::class, 'store'])->name('replies.store');
 
 Route::post('likes', LikeController::class)->name('likes.store');
+Route::post('answer/{thread:slug}', AnswerController::class)->name('answer.store');
 
 require __DIR__ . '/auth.php';
